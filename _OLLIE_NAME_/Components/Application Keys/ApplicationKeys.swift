ded25c52256e4ad641518ccf7bd401ab16c946bc
Jelle Vandebeeck <jelle@fousa.be>
2017-01-05 15:03:00 +0100

    Use our application keys mechanism.

---
+++ _OLLIE_NAME_/Components/Application Keys/ApplicationKeys.swift
@@ -0,0 +1,23 @@
+//
+//  ApplicationKeys.swift
+//  _OLLIE_NAME_
+//
+//  Created by Ollie on 05/01/17.
+//  Copyright © 2017 iCapps. All rights reserved.
+//
+
+import Stella
+
+struct ApplicationKeys {
+
+    // MARK: - Init
+
+    static let shared = ApplicationKeys()
+
+    // MARK: - Output
+
+    var printLevel: PrintLevel {
+        return .verbose
+    }
+    
+}
