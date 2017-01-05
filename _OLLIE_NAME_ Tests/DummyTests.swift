097a393115931618b5d1de949688414a15bf0c8f
Jelle Vandebeeck <jelle@fousa.be>
2017-01-05 13:50:27 +0100

    Add the test target.

---
+++ _OLLIE_NAME_ Tests/DummyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DummyTests.swift
+//  _OLLIE_NAME_ Tests
+//
+//  Created by Ollie on 05/01/2017.
+//  Copyright © 2017 iCapps. All rights reserved.
+//
+
+import XCTest
+
+class DummyTests: XCTestCase {
+    
+    func testExample() {
+        XCTAssert(false)
+    }
+    
+}
