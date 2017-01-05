3f16f96178b74d84f121e5ad38f626d4dae53677
Jelle Vandebeeck <jelle@fousa.be>
2017-01-05 15:49:51 +0100

    Linter the code.

--- _OLLIE_NAME_/AppDelegate.swift
+++ _OLLIE_NAME_/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool { // swiftlint:disable:this line_length
         // Set the correct print level.
         Output.level = ApplicationKeys.shared.printLevel
         
